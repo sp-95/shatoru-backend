57516f144d7d8ad96f3abb42213e24b495234547
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-06 12:21:16 -0500

    [UMDSS-25] [feat] Add sqlalchemy session

--- src/shatoru_backend/core/config.py
+++ src/shatoru_backend/core/config.py
@@ -1,9 +1,9 @@
 from functools import cache
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 
 from loguru._defaults import LOGURU_FORMAT
-from pydantic import AnyHttpUrl, BaseSettings
+from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn
 
 _BASE_PATH = Path(__file__).parent.parent.parent.parent
 
@@ -23,6 +23,13 @@ class Settings(BaseSettings):
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
     class Config:
         env_prefix = "SHATORU_"
