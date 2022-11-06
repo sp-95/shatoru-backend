fed1cf34a2deb21b7026fadf0820871de465d287
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-06 15:12:30 -0500

    [UMDSS-25] [chore] Read debug flag and secret key from `.env` file

--- shatoru_backend/base/settings.py
+++ shatoru_backend/base/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(ku=cc+j(vc^kp&cpe(-225ymy(x7jn0atg-&8#6&*q=42npc@"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS: List[str] = []
 
