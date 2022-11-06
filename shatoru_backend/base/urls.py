b8cf72603cee1e4a18af527baf9b33b8288713ec
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-06 15:20:26 -0500

    [UMDSS-25] [feat] Setup the base API settings

--- shatoru_backend/base/urls.py
+++ shatoru_backend/base/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+api_urlpatterns = [
+    path("stop/", include("apps.routing.api.urls")),
+]
+
+# API v1 URL endpoints
+urlpatterns += [
+    path("api/v1/", include(api_urlpatterns)),
+]
+
+urlpatterns = urlpatterns + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
