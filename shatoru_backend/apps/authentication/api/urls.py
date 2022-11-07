a64ca29dca0280c29d5f869c10af3da00874bc1d
Shashanka Prajapati <shashankap95@gmail.com>
2022-11-06 23:41:05 -0500

    [UMDSS-6] [feat] Delete Driver API

--- shatoru_backend/apps/authentication/api/urls.py
+++ shatoru_backend/apps/authentication/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import URLPattern, URLResolver, path
 from rest_framework.authtoken.views import obtain_auth_token
 
 from shatoru_backend.apps.authentication.api.views import (
+    DeleteDriverAPIView,
     DriverAPIView,
     RegisterDriverAPIView,
 )
@@ -12,4 +13,5 @@ urlpatterns: List[URLPattern | URLResolver] = [
     path("get-token/", obtain_auth_token, name="get_token"),
     path("driver/register/", RegisterDriverAPIView.as_view()),
     path("driver/<int:id>/", DriverAPIView.as_view()),
+    path("driver/<int:id>/delete/", DeleteDriverAPIView.as_view()),
 ]
--- shatoru_backend/apps/authentication/api/views.py
+++ shatoru_backend/apps/authentication/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from shatoru_backend.apps.authentication.api.serializer import (
@@ -16,6 +16,17 @@ class RegisterDriverAPIView(CreateAPIView):
 class DriverAPIView(RetrieveUpdateAPIView):
     lookup_field = "id"
     permission_classes = (IsAdminUser, IsAuthenticated)
+    serializer_class = DriverSerializer
+
+    def get_queryset(self):
+        driver_id = self.kwargs.get("id")
+        driver = User.objects.filter(id=driver_id)
+        return driver
+
+
+class DeleteDriverAPIView(DestroyAPIView):
+    lookup_field = "id"
+    permission_classes = (IsAdminUser,)
     serializer_class = DriverSerializer
 
     def get_queryset(self):
