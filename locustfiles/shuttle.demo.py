a3263423c80e60f7910b9f451f264a311793b94f
rosy <rosyshrestha028@gmail.com>
2023-04-12 12:53:18 -0400

    Update stress test for shuttles to not make request with tokens

--- locustfiles/shuttle.demo.py
+++ locustfiles/shuttle.demo.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from locust import HttpUser, between, task
 
@@ -6,29 +7,32 @@ from locust import HttpUser, between, task
 class ShuttleDemo(HttpUser):
     wait_time = between(1, 5)
 
-    def on_start(self):
-        # Authenticate with the API
-        response = self.client.post(
-            "/auth/login/",
-            data={
-                "username": "admin",
-                "password": "admin",
-            },
+    @task(1)
+    def get_shuttle(self):
+        shuttles_response = self.client.get(
+            "/shuttles/",
+        )
+        if shuttles_response.status_code != 200:
+            return
+        shuttles = json.loads(shuttles_response.content)
+        shuttle_id = random.choice(shuttles)["id"]
+        self.client.get(
+            f"/shuttles/{shuttle_id}/",
+        )
+
+    @task(4)
+    def get_schedule(self):
+        # First, retrieve a list of all schedules so we can choose one at random
+        response = self.client.get(
+            "/shuttles/schedules/",
         )
         if response.status_code != 200:
-            raise Exception("Failed to authenticate with the API")
-        self.access_token = json.loads(response.content)["token"]
+            return
+        schedules = json.loads(response.content)
+        if not schedules:
+            return
+        schedule_id = random.choice(schedules)["id"]
 
-    @task
-    def list_schedules(self):
         self.client.get(
-            "/shuttles/schedules/",
-            headers={"Authorization": f"Token {self.access_token}"},
-        )
-
-    @task(1)
-    def list_shuttles(self):
-        self.client.get(
-            "/shuttles/",
-            headers={"Authorization": f"Token {self.access_token}"},
+            f"/shuttles/schedules/{schedule_id}/",
         )
