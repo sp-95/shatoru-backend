9c75aba4bbf049a186faa7c4445b684c74217416
rosy <rosyshrestha028@gmail.com>
2023-04-12 10:46:09 -0400

    Create stress test for shuttle endpoint for demo

---
+++ locustfiles/shuttle.demo.py
@@ -0,0 +1,27 @@
+import json
+
+from locust import HttpUser, between, task
+
+
+class Shuttle(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        # Authenticate with the API
+        response = self.client.post(
+            "/auth/login/",
+            data={
+                "username": "admin",
+                "password": "admin",
+            },
+        )
+        if response.status_code != 200:
+            raise Exception("Failed to authenticate with the API")
+        self.access_token = json.loads(response.content)["token"]
+
+    @task(1)
+    def list_schedules(self):
+        self.client.get(
+            "/shuttles/schedules/",
+            headers={"Authorization": f"Token {self.access_token}"},
+        )
